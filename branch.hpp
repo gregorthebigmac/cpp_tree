503af5516298093b20f1df61bd41ad789d0f9ae7
gregorthebigmac <gregorthebigmac@gmail.com>
2019-03-17 17:58:53 -0500

    still trying to figure out how to go about the tree structure. I'm thinking I may not use the "branch" or even the "tree" objects. I may go with a self-contained "dir" structure.

--- branch.hpp
+++ branch.hpp
@@ -4,13 +4,17 @@
 #include "dir.hpp"
 
 class branch {
+
 public:
 	branch() {}		// ctor
 	~branch() {}	// dtor
 
 	void build_branch();
+	
 private:
-	std::vector<dir> m_dir_list;
+	std::string m_cwd;
+	branch * br_parent;
+	std::vector<branch*> br_child;
 };
 
 void branch::build_branch() {
