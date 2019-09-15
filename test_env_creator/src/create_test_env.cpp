c04422b058d5e1016faca5503598415736f61a0f
gregorthebigmac <gregorthebigmac@gmail.com>
2019-09-15 02:01:22 -0500

    started working on test environment creator

---
+++ test_env_creator/src/create_test_env.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+	cout << "test" << endl;
+	return 0;
+}
