35efa51f3c9491c882dbbb26b237e597ea52ef5a
gregorthebigmac <gregorthebigmac@gmail.com>
2019-09-15 02:17:59 -0500

    added script to auto-move the binary to the working dir and test it.

---
+++ move_bin_to_working_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv test_env_creator/create_test_env ~/test_dir/ \
+&& cd ~/test_dir \
+&& ./create_test_env
--- test_env_creator/src/create_test_env.cpp
+++ test_env_creator/src/create_test_env.cpp
@@ -12,10 +12,6 @@ using std::vector;
 command cmd;
 
 int main() {
-	vector<string> terminal_feedback, error_list;
-	cmd.exec("echo \"stuff\"", terminal_feedback, error_list);
-	for (int i = 0; i < terminal_feedback.size(); i++) {
-		cout << terminal_feedback[i];
-	}
+	
 	return 0;
 }
